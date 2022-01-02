e48c128c137351cbc5dfbf1982d78a10393531e4
Tyler Conti <tjconti12@gmail.com>
2022-01-01 20:25:41 -0500

    basic pawn movement

--- Source/FirstProject/Critter.cpp
+++ Source/FirstProject/Critter.cpp
@@ -4,6 +4,7 @@
 #include "Critter.h"
 #include "Components/StaticMeshComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
 
 // Sets default values
 ACritter::ACritter()
@@ -21,6 +22,9 @@ ACritter::ACritter()
 	Camera->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
 
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	CurrentVelocity = FVector(0.f);
+	MaxSpeed = 100.f;
 }
 
 // Called when the game starts or when spawned
@@ -35,6 +39,9 @@ void ACritter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+	SetActorLocation(NewLocation);
+
 }
 
 // Called to bind functionality to input
@@ -42,4 +49,17 @@ void ACritter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ACritter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ACritter::MoveRight);
+
+}
+
+void ACritter::MoveForward(float Value)
+{
+	CurrentVelocity.X = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+}
+
+void ACritter::MoveRight(float Value)
+{
+	CurrentVelocity.Y = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
 }
